2ca483fea7a97a085dfd3f3bd323d5c21c97d8f2
strawy12 <hwaga0714@gmail.com>
2022-09-01 01:51:41 +0900

    리드미 수정

---
+++ Programmers/README.md
@@ -0,0 +1,4 @@
+<div align = "center">
+# Progammers
+
+</div> <br><br>
--- Programmers/신규 아이디 추천.cpp
+++ Programmers/신규 아이디 추천.cpp
@@ -1,160 +1,78 @@
-#include <string>
-#include <vector>
+	#include <string>
+	#include <vector>
 
-using namespace std;
-
-string solution(string new_id);
-string ReplaceCapitalToSmall(string new_id);
-string LimitChar(string new_id);
-string ReduplicationFullstop(string new_id);
-string FirstLastCheck(string new_id);
-string NullCheck(string new_id);
-string CharacterNumLimit(string new_id);
-string MinimumCharacterLimit(string new_id);
+	using namespace std;
 
 
-string solution(string new_id) {
-	string answer = ReplaceCapitalToSmall(new_id);
-	answer = LimitChar(answer);
-	answer = ReduplicationFullstop(answer);
-	answer = FirstLastCheck(answer);
-	answer = NullCheck(answer);
-	answer = CharacterNumLimit(answer);
-	answer = MinimumCharacterLimit(answer);
-	return answer;
-}
+	string solution(string new_id) {
+		string answer = "";
 
-string ReplaceCapitalToSmall(string new_id)
-{
-	for (int i = 0; i < new_id.length(); i++)
-	{
-		if ('A' <= new_id[i] && new_id[i] <= 'Z')
+		for (int i = 0; i < new_id.length(); i++)
 		{
-			new_id[i] += 32;
-		}
-	}
+			char c = new_id[i];
 
-
-	return new_id;
-}
-
-string LimitChar(string new_id)
-{
-
-	string result = "";
-	for (int i = 0; i < new_id.length(); i++)
-	{
-		if ('a' <= new_id[i] && new_id[i] <= 'z')
-		{
-			result += new_id[i];
-		}
-
-		if ('0' <= new_id[i] && new_id[i] <= '9')
-		{
-			result += new_id[i];
-		}
-
-		if (new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.')
-		{
-			result += new_id[i];
-		}
-	}
-
-
-	return result;
-}
-
-string ReduplicationFullstop(string new_id)
-{
-
-	string result = "";
-
-	for (int i = 0; i < new_id.length(); i++)
-	{
-		if (new_id[i] == '.')
-		{
-			if (new_id[i + 1] == '.')
+			if ('0' <= c && c <= '9' && 'a' <= c && c <= 'z')
 			{
-				int j = 0;
-				for (j = i; j < new_id.length(); j++)
+				answer += c;
+			}
+
+			else if ('A' <= c && c <= 'Z')
+			{
+				answer += tolower(c);
+			}
+
+			else
+			{
+				switch (c)
 				{
-					if (new_id[j] != '.')
-					{
-						break;
-					}
+				case '.':
+				{
+					if (answer.size() != 0 && answer.back() == '.')
+						continue;
+					answer += c;
+					break;
 				}
 
-				i = j - 1;
+				case '-':
+				case '_':
+					answer += c;
+					break;
+
+				case ' ':
+					answer += 'a';
+					break;
+				}
+			}
+
+		}
+
+		if (answer.front() == '.')
+		{
+			answer.erase(answer.begin());
+		}
+
+		if (answer.back() == '.')
+		{
+			answer.erase(answer.end() - 1);
+		}
+
+		if (answer.length() > 15)
+		{
+				answer.erase(answer.begin() + 15, answer.end());
+
+			if (answer.back() == '.')
+			{
+				answer.erase(answer.end() - 1);
 			}
 		}
 
-
-		result += new_id[i];
-	}
-
-
-	return result;
-}
-
-string FirstLastCheck(string new_id)
-{
-	string result = "";
-
-	for (int i = 0; i < new_id.length(); i++)
-	{
-		if (new_id[i] == '.' && (i == 0 || i == new_id.length() - 1))
+		else if (answer.length() < 3)
 		{
-
-			continue;
+			while (answer.length() < 3)
+			{
+				answer.push_back(answer.back());
+			}
 		}
 
-		result += new_id[i];
+		return answer;
 	}
-
-
-	return result;
-}
-
-string NullCheck(string new_id)
-{
-
-	if (new_id == "")
-	{
-		new_id = "a";
-	}
-
-
-	return new_id;
-}
-
-//6´Ü°è Â÷·Ê
-string CharacterNumLimit(string new_id)
-{
-	if (new_id.length() > 15)
-	{
-		string result = "";
-		for (int i = 0; i < 15; i++)
-		{
-			result += new_id[i];
-		}
-		new_id = result;
-	}
-	new_id = FirstLastCheck(new_id);
-
-	return new_id;
-}
-
-string MinimumCharacterLimit(string new_id)
-{
-	if (new_id.length() < 3)
-	{
-		char retChar = new_id[new_id.length() - 1];
-		for (int i = 0; new_id.length() < 3; i++)
-		{
-			new_id = new_id + retChar;
-		}
-	}
-
-
-	return new_id;
-}
